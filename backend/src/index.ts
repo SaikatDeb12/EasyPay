4529b9e1fc79ed9ebc10103b65ba15bca4855880
SaikatDeb12 <saikat.rcld@gmail.com>
2025-06-17 16:55:59 +0530

    setting up the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import connectdb from "./connectdb";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connectdb();
+
+app.listen(8000, () => console.log("Server started at: ", 8000));
