a161385ae73c357d406bb4786e8f4d82e28177da
SaikatDeb12 <saikat.rcld@gmail.com>
2025-07-08 21:08:14 +0530

    changed footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import { FaGithub, FaGoogle } from "react-icons/fa";
+// import { FaGithub, FaGoogle } from "react-icons/fa";
 
 interface PropsType {
   msg: string;
@@ -9,13 +9,13 @@ interface PropsType {
 const Footer: React.FC<PropsType> = ({ msg, redirect, onClick }) => {
   return (
     <div className="mx-10 w-full my-2 space-y-4">
-      <div className="flex justify-center items-center">
+      {/* <div className="flex justify-center items-center">
         <p className="text-stone-600">or continue with</p>
       </div>
       <div className="flex justify-evenly">
         <FaGithub className="border-2 border-stone-300 w-40 cursor-pointer hover:border-blue-400 h-8 py-1 text-stone-500 bg-transparent rounded-lg" />
         <FaGoogle className="border-2 border-stone-300 w-40 cursor-pointer hover:border-blue-400 h-8 py-1 text-stone-500 bg-transparent rounded-lg" />
-      </div>
+      </div> */}
       <div className="text-center">
         <span>{msg}</span>
         <span
